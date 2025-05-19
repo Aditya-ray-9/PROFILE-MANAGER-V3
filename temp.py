4c06d7a9dc6955da7ccec982295a36fe9b2e626b
Aditya-ray-999 <168975407+Aditya-ray-999@users.noreply.github.com>
2025-05-19 19:34:34 +0530

    Overwrite remote with latest local version

---
+++ temp.py
@@ -0,0 +1,5 @@
+id = ""
+app_name = "Profile-Manager"
+app_version = "V3.1"
+git = ""
+languages = ["HTML", "JS(JAVASCRIPT)", "TS(TYPESCRIPT)", "CSS(TAILWIND)", "PYTHON", "JSON", "TSX(REACT)", "PROFILE"]
